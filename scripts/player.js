61e9a9086e876d8776591b5d853119582d1f8050
PeRiOuS-AD <aminedridi181@gmail.com>
2025-12-01 11:59:25 +0100

    player.js updated functions can_dash/update/move...

--- scripts/player.js
+++ scripts/player.js
@@ -1,4 +1,12 @@
 class Player {
+    position = {
+        x: 0,
+        y: 0
+    };
+    vitesse = {
+        vx: 0,
+        vy: 0
+    };
     constructor(name, vitesse, position) {
         this.name = name;
         this.score = 0;
@@ -7,12 +15,18 @@ class Player {
         this.jett_dash = game_level == 3 ? true : false;
     }
 
-    update() {
+    update(vitesse_updated, pos_updated, last_time_dashed) {
         // updates everything ok
+        this.score += 10; //the incrementation of the score i 've put as to be seen afterwards.
+        this.vitesse = vitesse_updated;
+        this.position = pos_updated;
+        this.jett_dash = (Date.getTime() - last_time_dashed) >= 10 * 1000 ? true : false;
     }
 
-    move(direction) {
+    move(direction_x, direction_y, time) {
         // calculates the new direction of movement
+        this.position.x += direction_x * time * this.vitesse.vx
+        this.position.y += direction_y * time * this.vitesse.vy
     }
 
     dash() {
@@ -20,17 +34,17 @@ class Player {
     }
 
     // shoot() {}
-    
-    canDash() {
 
+    canDash() {
+        return this.jett_dash
     }
 
     hit() {
-        //when the colissionmanager detects a 
+        //when the colissionmanager detects a collision
     }
 
     getBounds() {
         // get player dimensions and position delimitation
-            
+
     }
 }
